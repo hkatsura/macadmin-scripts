7e2c165cf019c035a75198c376f767616546c95c
hkatsura <35199431+hkatsura@users.noreply.github.com>
2025-06-24 10:01:24 -0700

    fix typo (comma)

--- installinstallmacos.py
+++ installinstallmacos.py
@@ -66,12 +66,12 @@ DEFAULT_SUCATALOGS = {
     '23': 'https://swscan.apple.com/content/catalogs/others/'
           'index-14-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9'
           '-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog',
-    '24': 'https://swscan.apple.com/content/catalogs/others/'
+    '24': 'https://swscan.apple.com/content/catalogs/others/',
           'index-15-14-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9'
           '-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog',
     '25': 'https://swscan.apple.com/content/catalogs/others/'
           'index-26-15-14-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9'
-          '-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog',
+          '-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog'
 }
 
 SEED_CATALOGS_PLIST = (
